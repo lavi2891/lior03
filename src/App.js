71f3138c619a53086d247dce2d469079b16c3ea6
Lavi Hen-Zion <lavi2891@gmail.com>
2023-08-30 11:01:41 +0300

    app.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
-import React, { useState } from 'react';
-import TeamSelect from './components/TeamSelect';
-import MeetingsList from './components/MeetingsList';
-import AddMeetingForm from './components/AddMeetingForm';
+import React, { useState } from 'react'
+import TeamSelect from './components/TeamSelect'
+import MeetingsList from './components/MeetingsList'
+import AddMeetingForm from './components/AddMeetingForm'
 
 function App() {
-  const [selectedTeam, setSelectedTeam] = useState('');
+    const [selectedTeam, setSelectedTeam] = useState('')
+    const [refreshMeetingsList, setRefreshMeetingsList] = useState(Math.random().toString(36).substring(2,6))
 
-  return (
-    <div>
-      <TeamSelect />
-      <MeetingsList selectedTeam={selectedTeam} />
-      <AddMeetingForm />
-    </div>
-  );
+    // Define a function to handle team selection
+    const handleTeamSelect = (team) => {
+        setSelectedTeam(team)
+    }
+    const forceRefreshMeetingsList = () =>
+        setRefreshMeetingsList(Math.random().toString(36).substring(2,6))
+
+    return (
+        <div>
+            <TeamSelect onSelect={handleTeamSelect} />
+            {selectedTeam && <MeetingsList selectedTeam={selectedTeam} refreshMeetingsList={refreshMeetingsList} />}
+            {selectedTeam && ( <AddMeetingForm selectedTeam={selectedTeam} forceRefreshMeetingsList={forceRefreshMeetingsList}  />
+            )}
+        </div>
+    )
 }
 
-export default App;
+export default App
