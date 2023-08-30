78f73d4d90d0f1a33108a933f4e14cb4a432434e
Lavi Hen-Zion <lavi2891@gmail.com>
2023-08-30 11:05:05 +0300

    MeetingsList style, field force refresh

--- src/components/MeetingsList.js
+++ src/components/MeetingsList.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect, useCallback } from 'react';
+import 'C:/Users/Lavi/OneDrive/שולחן העבודה/מטלות ברוכים/Task 03/task03/src/style/MeetingList.css';
 
-function MeetingsList({ selectedTeam }) {
+function MeetingsList({ selectedTeam, refreshMeetingsList }) {
   const [meetings, setMeetings] = useState([]);
 
   // Define fetchMeetings as a memoized function
   const fetchMeetings = useCallback(async () => {
     try {
-      const response = await fetch(`http://localhost:3001/api/meetings?team_code=${selectedTeam}`);
+      const response = await fetch(`http://localhost:3001/api/meetings/${selectedTeam}`);
       if (!response.ok) {
         throw new Error('Network response was not ok');
       }
@@ -22,25 +23,39 @@ function MeetingsList({ selectedTeam }) {
     if (selectedTeam) {
       fetchMeetings();
     }
-  }, [selectedTeam, fetchMeetings]); // Include selectedTeam and fetchMeetings in the dependency array
+  }, [selectedTeam, refreshMeetingsList, fetchMeetings]); // Include selectedTeam and fetchMeetings in the dependency array
 
   return (
-    <div>
+    <div key={refreshMeetingsList} className="meetings-list">
       <h2>Meetings for {selectedTeam}</h2>
-      <ul>
-        {meetings.map((meeting) => (
-          <li key={meeting.id}>
-            <strong>{meeting.title}</strong>
-            <p>Start date: {meeting.start_date}</p>
-            <p>Start ime: {meeting.start_time}</p>
-            <p>End date: {meeting.end_date}</p>
-            <p>End time: {meeting.end_time}</p>
-            <p>Location: {meeting.location}</p>
-            <p>Meeting room: {meeting.meeting_room}</p>
-            <p>Description: {meeting.description}</p>
-          </li>
-        ))}
-      </ul>
+      <table>
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Start Date</th>
+            <th>Start Time</th>
+            <th>End Date</th>
+            <th>End Time</th>
+            <th>Location</th>
+            <th>Meeting Room</th>
+            <th>Description</th>
+          </tr>
+        </thead>
+        <tbody>
+          {meetings.map((meeting) => (
+            <tr key={meeting.id}>
+              <td>{meeting.title}</td>
+              <td>{new Date(meeting.start_date).toLocaleDateString('he-IL')}</td>
+              <td>{meeting.start_time}</td>
+              <td>{new Date(meeting.end_date).toLocaleDateString('he-IL')}</td>
+              <td>{meeting.end_time}</td>
+              <td>{meeting.location}</td>
+              <td>{meeting.meeting_room}</td>
+              <td>{meeting.description}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
